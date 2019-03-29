c7b7fee93fad9d907464c04520e0c35670fbed9b
Zhenissyntax <zhenisismagambetov@gmail.com>
2019-03-28 21:59:28 -0400

    Codes

---
+++ src/com/class12/Springreview.java
@@ -0,0 +1,20 @@
+package com.class12;
+public class Springreview{
+	public static void main(String [] args) {
+		String day="Sunday";
+		for(int i=day.length()-1;i>=0;i--) {
+			System.out.print(day.charAt(i));
+			System.out.println();
+			String str="Today is warm";
+			
+				if (!(str.isEmpty())) {
+					if(str.length()%2!=0 && str.length()>=3) {
+						System.out.println(str.charAt(str.length()/2));
+					}
+				}
+			}
+			
+				
+			}
+		}
+	
